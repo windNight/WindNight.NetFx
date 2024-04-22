603548159b641e2a9d61ee2308c08cfe22a66313
windnight <windnight27@gmail.com>
2024-04-22 09:26:14 +0800

    feat:add WindNight.Extension.Dapper.Mssql

---
+++ src/Dapper/WindNight.Extension.Dapper.Mssql/Internals/ConfigItems.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection.WnExtension;
+
+namespace WindNight.Extension.Dapper.Mssql.@internal
+{
+    internal class ConfigItems //: ConfigItemsBase
+    {
+        public static bool OpenDapperLog => GetConfigBoolValue(ConfigItemsKey.OpenDapperLogKey);
+        //{
+        //    get
+        //    {
+        //        var config = Ioc.Instance.CurrentConfigService;// Ioc.GetService<IConfigService>();
+        //        if (config == null)
+        //        {
+
+        //            return false;
+        //        }
+
+        //        return config.Configuration.GetSection(ConfigItemsKey.OpenDapperLogKey).Get<bool>() ||
+        //               config.GetAppSetting(ConfigItemsKey.OpenDapperLogKey, false, false);
+
+        //    }
+        //}
+
+        public static bool IsLogConnectString => GetConfigBoolValue(ConfigItemsKey.IsLogConnectStringKey);
+        public static int DapperWarnMs => GetConfigIntValue(ConfigItemsKey.DapperWarnMsKey, 100);
+        //{
+        //    get
+        //    {
+        //        return GetConfigBoolValue(ConfigItemsKey.IsLogConnectStringKey);
+        //        // var config = Ioc.Instance.CurrentConfigService;
+        //        // if (config == null) return false;
+        //        // return config.GetAppSetting(ConfigItemsKey.IsLogConnectStringKey, false, false);
+        //    }
+        //}
+
+        static bool GetConfigBoolValue(string key)
+        {
+            var config = Ioc.Instance.CurrentConfigService;
+            if (config == null) return false;
+
+            return config.Configuration.GetSection(key).Get<bool>() ||
+                   config.GetAppSetting(key, false, false);
+
+        }
+
+        static int GetConfigIntValue(string key, int defaultValue = 0)
+        {
+            try
+            {
+                var config = Ioc.Instance.CurrentConfigService;
+                if (config == null) return defaultValue;
+                var value1 = config.Configuration.GetSection(key).Get<int>();
+                var value2 = config.GetAppSetting(key, defaultValue, false);
+                return Math.Max(value2, value1);
+            }
+            catch (Exception ex)
+            {
+                return defaultValue;
+            }
+
+        }
+
+        static class ConfigItemsKey
+        {
+            internal static string DapperWarnMsKey = "DapperConfig:WarnMs";
+            internal static string OpenDapperLogKey = "DapperConfig:OpenDapperLog";
+            internal static string IsLogConnectStringKey = "DapperConfig:IsLogConnectString";
+
+        }
+
+    }
+}
---
+++ src/Dapper/WindNight.Extension.Dapper.Mssql/Mssql/SqlServerBaseWithId.cs
@@ -0,0 +1,116 @@
+﻿using Newtonsoft.Json.Extension;
+using WindNight.Extension.Dapper.Mssql.@internal;
+
+namespace WindNight.Extension.Dapper.Mssql
+{
+    public abstract partial class SqlServerBase<TEntity, TId>
+    {
+        protected string InsertSql =>
+            $@"INSERT INTO {BaseTableName}({InsertTableColumns})
+   VALUES ({InsertTableColumnValues});
+   SELECT @@identity;";
+
+        protected string BatchInsertSql =>
+            $@"INSERT INTO {BaseTableName}({InsertTableColumns})
+   VALUES ({InsertTableColumnValues})";
+
+
+        /// <summary>
+        ///  使用<see cref="BatchInsertSql"/> 语句批量插入
+        /// </summary>
+        /// <param name="insertList"></param>
+        /// <returns></returns>
+        public bool BatchInsertUseValues(IList<TEntity> insertList)
+        {
+            if (insertList == null || !insertList.Any()) return false;
+
+            var flag = DbExecute(BatchInsertSql, insertList.ToArray()) > 0;
+            if (!flag)
+                LogHelper.Warn($"Insert Into {BaseTableName} handler error ,entities is {insertList.ToJsonStr()} . ",
+                    appendMessage: false);
+            return flag;
+        }
+
+        /// <summary>
+        /// 使用<see cref="BatchInsertSql"/> 语句批量插入 异步
+        /// </summary>
+        /// <param name="insertList"></param>
+        /// <returns></returns>
+        public async Task<bool> BatchInsertUseValuesAsync(IList<TEntity> insertList)
+        {
+            if (insertList == null || !insertList.Any()) return false;
+
+            var flag = await DbExecuteAsync(BatchInsertSql, insertList.ToArray()) > 0;
+            if (!flag)
+                LogHelper.Warn($"Insert Into {BaseTableName} handler error ,entities is {insertList.ToJsonStr()} . ",
+                    appendMessage: false);
+            return flag;
+        }
+
+        /// <summary>
+        /// 使用自定义语句批量插入
+        /// </summary>
+        /// <param name="insertSql"></param>
+        /// <param name="insertList"></param>
+        public void BatchInsert(string insertSql, IList<TEntity> insertList)
+        {
+            if (insertList == null || !insertList.Any()) return;
+
+            var flag = DbExecute(insertSql, insertList.ToArray()) > 0;
+            if (!flag)
+                LogHelper.Warn($"Insert Into {BaseTableName} handler error ,entities is {insertList.ToJsonStr()} . ",
+                    appendMessage: false);
+        }
+
+        /// <summary>
+        /// 按条一条一条插入
+        /// </summary>
+        /// <param name="insertList"></param>
+        public void ListInsertOneByOne(IList<TEntity> insertList)
+        {
+            var insertSql =
+                $@"INSERT INTO {BaseTableName}({InsertTableColumns})
+   SELECT {InsertTableColumnValues}
+   FROM {BaseTableName}
+   WHERE NOT EXISTS(SELECT 1 FROM {BaseTableName} WHERE {EqualEntityCondition})";
+
+            Parallel.ForEach(insertList, item =>
+            {
+                var flag = DbExecute(insertSql, item) > 0;
+                if (!flag)
+                    LogHelper.Warn($"Insert Into {BaseTableName} handler Failed ,entity is {item.ToJsonStr()} . ",
+                        appendMessage: false);
+            });
+        }
+
+        /// <summary>
+        /// 使用并行任务执行update 慎用
+        /// </summary>
+        /// <param name="updateSql"></param>
+        /// <param name="updateList"></param>
+        /// <returns></returns>
+        public bool BatchUpdate(string updateSql, IList<TEntity> updateList)
+        {
+            if (updateList == null || !updateList.Any()) return false;
+            Parallel.ForEach(updateList, item =>
+            {
+                var flag = DbExecute(updateSql, item) > 0;
+                if (!flag)
+                    LogHelper.Warn($"Update {BaseTableName} handler Failed ,entity is {item.ToJsonStr()} . ",
+                        appendMessage: false);
+            });
+            return true;
+        }
+
+        /// <summary>
+        ///  带重试机制的批量插入
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="retryCount"></param>
+        public void BatchInsertWithRetry(Action action, int retryCount = 3)
+        {
+            DoRetryWhenHandlerSocketException(action, $"BatchInsert_{BaseTableName}", retryCount);
+        }
+    }
+
+}
