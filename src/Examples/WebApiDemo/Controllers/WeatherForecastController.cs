ed0edec1b006ebbe57b1ad5a12ab5c05fc07087f
windnight <windnight27@gmail.com>
2025-02-24 08:27:17 +0800

    feat:demo sync;

--- src/Examples/WebApiDemo/Controllers/WeatherForecastController.cs
+++ src/Examples/WebApiDemo/Controllers/WeatherForecastController.cs
@@ -10,34 +10,112 @@ using WindNight.LogExtension;
 namespace WebApiDemo.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/test")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly string[] Summaries =
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+
+        private readonly Dictionary<string, string> SignDict = new()
+        {
+            { "Authorization", "格式 Bearer xx" },
+            { "AppId", "AppId" },
+            { "AppCode", "AppCode" },
+            { "AppToken", "Sign" },
+            { "Ts", "当前时间戳" },
+        };
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        private string GetHeaderData(HttpRequest httpRequest, string headerName)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            if (httpRequest.Headers.TryGetValue(headerName, out var requestHeader))
             {
-                Date = DateOnly.FromDateTime(HardInfo.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                var header = requestHeader.FirstOrDefault();
+                if (!header.IsNullOrEmpty())
+                {
+                    return header.Trim();
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private Dictionary<string, string> GetAllHeaderData(HttpRequest httpRequest)
+        {
+            var dict = new Dictionary<string, string>();
+            foreach (var item in httpRequest.Headers)
+            {
+                dict.Add(item.Key, item.Value);
+            }
+
+            return dict;
+        }
+
+        [HttpGet("tt")]
+        [NonAuth]
+        public object Get()
+        {
+            var signData = new Dictionary<string, string>();
+
+            foreach (var item in SignDict)
+            {
+                var data = GetHeaderData(Request, item.Key);
+                signData.Add(item.Key, data);
+            }
+
+            var rangeData = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                })
+                .ToArray();
+            var allHeaderData = GetAllHeaderData(Request);
+            return new { allHeaderData, signData, rangeData };
         }
 
 
+        [HttpGet("t")]
+        public object Get111()
+        {
+            var signData = new Dictionary<string, string>();
+
+            foreach (var item in SignDict)
+            {
+                var data = GetHeaderData(Request, item.Key);
+                signData.Add(item.Key, data);
+            }
+
+            var rangeData = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                })
+                .ToArray();
+            return new { signData, rangeData };
+        }
+
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> GetWeatherForecast()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(HardInfo.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                })
+                .ToArray();
+        }
 
 
         [HttpGet("log/config")]
@@ -51,26 +129,23 @@ namespace WebApiDemo.Controllers
                 return new
                 {
                     HardInfo.OperatorSys,
-                    LogConfigPath =  LogHelper.CurrentLog4NetConfigPath,
-                    DcLogOptions = dcLogOption
-
+                    LogConfigPath = LogHelper.CurrentLog4NetConfigPath,
+                    DcLogOptions = dcLogOption,
                 };
-
             }
             catch (Exception ex)
             {
                 return ex;
             }
-
         }
 
         [HttpGet("log/push")]
         [NonAuth]
         public object TestLogPush()
         {
-            LogHelper.Info($"test info log store LogHelper ");
+            LogHelper.Info("test info log store LogHelper ");
 
-            DcLogHelper.Info($"test info log store DcLogHelper ");
+            DcLogHelper.Info("test info log store DcLogHelper ");
 
             Ioc.GetService<ILogService>()?.Info("test info log store ILogService");
 
@@ -78,7 +153,5 @@ namespace WebApiDemo.Controllers
 
             return true;
         }
-
-
     }
 }
--- src/Examples/WebApiDemo/Startup.cs
+++ src/Examples/WebApiDemo/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using Microsoft.AspNetCore.Hosting.WnExtensions;
 using WindNight.Config.Abstractions;
 using WindNight.Config.Extensions;
@@ -7,12 +7,27 @@ namespace WebApiDemo
 {
     public partial class Startup : WebStartupBase
     {
+        protected override Action<IApplicationBuilder> SelfMiddlewareAction => app =>
+        {
+            var swaggerSignDict = SelfSwaggerAuthDictFunc.Invoke();
+            app.UseMiddleware<SelfSwaggerSignValidMiddleware>(swaggerSignDict);
+
+        };
+
+        protected override Func<Dictionary<string, string>> SelfSwaggerAuthDictFunc => () => new Dictionary<string, string>
+        {
+            {"Authorization","格式 Bearer xx"},
+            {"AppId","执行的AppId"},
+            {"AppCode","执行的AppCode"},
+            {"AppToken","当前请求的Token"},
+            {"Ts","当前时间戳"},
+        };
+
         protected override string NamespaceName => Assembly.GetEntryAssembly()?.FullName;
 
         public Startup(IConfiguration configuration) : base(configuration)
         {
         }
-
 
         protected override void UseBizConfigure(IApplicationBuilder app)
         {
@@ -20,7 +35,6 @@ namespace WebApiDemo
 
 
         }
-
 
         protected override void ConfigBizServices(IServiceCollection services)
         {
