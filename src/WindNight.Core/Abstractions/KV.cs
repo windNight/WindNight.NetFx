509c5d4c0af1c18ef363dc1e418cdb78508021fe
windnight <windnight27@gmail.com>
2025-03-14 16:16:41 +0800

    feat: KV add  override

--- src/WindNight.Core/Abstractions/KV.cs
+++ src/WindNight.Core/Abstractions/KV.cs
@@ -19,6 +19,11 @@ namespace WindNight.Core.Abstractions
     {
         public T Key { get; set; }
         public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"Key:{Key},Value:{Value}";
+        }
     }
 
     public class KV : KV<int>, IKV
